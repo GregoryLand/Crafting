99d9d2c1ef460f0d76f67f67da8834a7e7c8320a
Gregory Land <landjgregory@gmail.com>
2016-03-24 18:04:11 -0400

    Bugfixes to get first compile working

--- Crafting.lua
+++ Crafting.lua
@@ -1,29 +1,33 @@
 -- Functions to handle low level crafting and present a nice interface
-RECIPE_DIR = "./Recipe"
+RECIPE_DIR = "./Recipes"
 INGREDIENT_NAME_DELIMITER = ':'
 
 -- Return an array with the name of each item indexed by grid location 0-8
 -- Example for stick
 -- [Stick, nil, nil, Stick, nil, nil, nil, nil, nil]
 function LookupRecipe(ItemName)
+  craftingArray = {}
+
   -- Create Blank Array
   for i=0,8 do
     craftingArray[i] = nil
   end
 
   -- Open the file
-  if type(ItemName) ~= string then
+  if type(ItemName) ~= "string" then
     return "Error: Bad Recipe Name"
   end
 
-  recipeFile = fs.open(RECIPE_DIR .. "/" .. ItemName)
+  recipeFile = fs.open(RECIPE_DIR .. "/" .. ItemName, 'r')
   -- Read till the end of the file
   repeat
      ingredient = recipeFile.readLine()
      
      if ingredient ~= nil then
        -- Read each ingredient name
-       ingredientName = ingredient.sub( 0, ingredient.match(INGREDIENT_NAME_DELIMITER))
+       print( ingredient.match("^" .. INGREDIENT_NAME_DELIMITER .. "$"))
+       print( ingredient.sub(0, ( ingredient.match("^" .. INGREDIENT_NAME_DELIMITER .. "$"))))
+       ingredientName = ingredient.sub( 0, ingredient.match("^" .. INGREDIENT_NAME_DELIMITER .. "$"))
        -- Read each location for that item and write the item name into the array location
        for gridLocation in ingredient.gmatch("^[0-8],$") do
         -- If not nil recipe is bad and has two items existing in the same grid location
